53a76ba7b6315edf2ebf72ad75a7be01730354c7
= <ari.weitzman@gmail.com>
2015-07-14 12:31:56 -0400

    migrate games table, add index's to users table

--- db/migrate/20150714162101_create_games.rb
+++ db/migrate/20150714162101_create_games.rb
@@ -1,6 +1,11 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
+      t.string :title, null: false
+      t.string :company
+      t.string :genre, null: false
+      t.integer :avg_rating, null: false
+      t.text :description
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714143233) do
+ActiveRecord::Schema.define(version: 20150714162329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.string   "company"
+    t.string   "genre",       null: false
+    t.integer  "avg_rating",  null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
@@ -24,5 +34,8 @@ ActiveRecord::Schema.define(version: 20150714143233) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
 end
