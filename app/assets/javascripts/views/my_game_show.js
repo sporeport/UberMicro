ef087a1e25d05fafb2522924ed0fdaafdc2ed8e8
Sam Hone-Studer <swhstuder@gmail.com>
2015-07-16 16:59:27 -0400

    format myGames page.

--- app/assets/javascripts/views/my_game_show.js
+++ app/assets/javascripts/views/my_game_show.js
@@ -1,3 +1,9 @@
 UberMicro.Views.MyGameShow = Backbone.View.extend({
-  
+
+  template: JST["my_games/my_game_show"],
+
+  render: function () {
+    this.$el.html(this.template({ myGame: this.model }))
+    return this;
+  }
 })
--- app/assets/javascripts/views/my_games_index.js
+++ app/assets/javascripts/views/my_games_index.js
@@ -1,29 +1,27 @@
 UberMicro.Views.MyGamesIndex = Backbone.CompositeView.extend({
 
-  initialize: function () {
+  initialize: function (options) {
     this.listenTo(this.collection, "sync", this.render);
   },
 
+  className: "my-games-container",
+
+  template: JST["my_games/my_games_index"],
+
   addSubViews: function () {
     this.collection.forEach(function (model) {
-      var showView = new UberMicro.Views.GameShow({
+      var showView = new UberMicro.Views.MyGameShow({
         model: model,
-        collection: this.collection,
+        collection: this.collection
       });
 
-      this.addSubview(".games-list", showView);
+      this.addSubview(".my-games-list", showView);
     }.bind(this))
   },
 
   render: function () {
     this.$el.html(this.template());
     this.addSubViews();
-    return this;
-  }
-
-
-  render: function () {
-    this.$el.html(this.template({ myGames: this.collection }))
     return this;
   }
 })
