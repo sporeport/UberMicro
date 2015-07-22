151385526ad19613ee24dd8828f9466bf1e5206f
Sam Hone-Studer <swhstuder@gmail.com>
2015-07-22 16:48:26 -0400

    Navigate to show page after adding a game from gb.

--- app/assets/javascripts/models/gbGame.js
+++ app/assets/javascripts/models/gbGame.js
@@ -5,6 +5,9 @@ UberMicro.Models.GbGame = Backbone.Model.extend({
       url: "/api/games",
       method: "POST",
       data: { gbid: this.get("gbid") },
+      success: function (data) {
+        Backbone.history.navigate("#/games/" + data.id, { trigger: true })
+      }
     })
   }
 })
